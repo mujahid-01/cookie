885645ec29cf41c502df71cacad575923eafea19
mujahid-01 <69314872+mujahid-01@users.noreply.github.com>
2021-05-24 22:03:37 +0530

    Add files via upload

---
+++ cookie.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main(void) {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    int n,flag=1;
+	    cin>>n;
+	    string s[n];
+	    for(int i=0;i<n;i++)
+	    cin>>s[i];
+	    for(int i=0;i<n-1;i++)
+	    {
+	        if(s[i]==s[i+1] && s[i]=="cookie" || s[n-1]=="cookie")
+	        flag=0;
+	    }
+	    if(n==1 && s[0]=="cookie")
+	    flag=0;
+	    if(flag==1)
+	    cout<<"YES"<<endl;
+	    else
+	    cout<<"NO"<<endl;
+	}
+	return 0;
+	}
